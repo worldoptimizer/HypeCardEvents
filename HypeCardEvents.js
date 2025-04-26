a1c47846050e1a54438f02b39d334948b5aa99e2
Max Ziebell <1752114+worldoptimizer@users.noreply.github.com>
2025-04-26 16:42:21 +0200

    v1.0.4

--- HypeCardEvents.js
+++ HypeCardEvents.js
@@ -1,12 +1,15 @@
 /*!
- * Hype Card Events 1.0.1
+ * Hype Card Events 1.0.4
  * copyright (c) 2025 by Max Siebell (https://maxziebell.de). MIT-license
  */
 
 /*
  * Version-History
  * 1.0.0 Initial release under MIT-license
- * 1.0.1 Include oldName and newName in event payloads
+ * 1.0.1 Added initial-load support
+ * 1.0.2 Consolidated unload/load logic into HypeSceneLoad
+ * 1.0.3 Introduced event payloads with oldName and newName
+ * 1.0.4 Added HypeCardPrepare, consolidated helper, and generic fallback logic
  */
 
 if ("HypeCardEvents" in window === false) {
@@ -14,47 +17,52 @@ if ("HypeCardEvents" in window === false) {
 
         var prevCardByDoc = {};
 
+        /**
+         * Fires specific handler and generic HypeCardEvent if defined
+         */
+        function _fireEvent(hypeDocument, element, eventType, previousCardName, currentCardName) {
+            var hypeFunctions = hypeDocument.functions();
+            var payload = { type: eventType, previousCardName: previousCardName, currentCardName: currentCardName };
+
+            if (typeof hypeFunctions[eventType] === 'function') {
+                hypeFunctions[eventType](hypeDocument, element, payload);
+            }
+            if (typeof hypeFunctions.HypeCardEvent === 'function') {
+                hypeFunctions.HypeCardEvent(hypeDocument, element, payload);
+            }
+        }
+
+        function HypeScenePrepareForDisplay(hypeDocument, element, event) {
+            var docId = hypeDocument.documentId();
+            var previousCardName = prevCardByDoc[docId];
+            var currentCardName = hypeDocument.currentSceneName();
+
+            if (previousCardName === undefined || currentCardName !== previousCardName) {
+                _fireEvent(hypeDocument, element, "HypeCardPrepare", previousCardName, currentCardName);
+            }
+        }
+
         function HypeSceneLoad(hypeDocument, element, event) {
             var docId = hypeDocument.documentId();
-            var oldName = prevCardByDoc[docId];
-            var newName = hypeDocument.currentSceneName();
-            var hypeFunctions = hypeDocument.functions();
+            var previousCardName = prevCardByDoc[docId];
+            var currentCardName = hypeDocument.currentSceneName();
 
-            // initial load
-            if (oldName === undefined) {
-                var loadEvent = { type: "HypeCardLoad", oldName: undefined, newName: newName };
-                if (hypeFunctions.HypeCardLoad) {
-                    hypeFunctions.HypeCardLoad(hypeDocument, element, loadEvent);
-                } else if (hypeFunctions.HypeCardEvent) {
-                    hypeFunctions.HypeCardEvent(hypeDocument, element, loadEvent);
+            if (previousCardName === undefined || currentCardName !== previousCardName) {
+                if (previousCardName !== undefined) {
+                    _fireEvent(hypeDocument, element, "HypeCardUnload", previousCardName, currentCardName);
                 }
+                _fireEvent(hypeDocument, element, "HypeCardLoad", previousCardName, currentCardName);
+                prevCardByDoc[docId] = currentCardName;
             }
-            // subsequent scene changes
-            else if (newName !== oldName) {
-                var unloadEvent = { type: "HypeCardUnload", oldName: oldName, newName: newName };
-                if (hypeFunctions.HypeCardUnload) {
-                    hypeFunctions.HypeCardUnload(hypeDocument, element, unloadEvent);
-                } else if (hypeFunctions.HypeCardEvent) {
-                    hypeFunctions.HypeCardEvent(hypeDocument, element, unloadEvent);
-                }
-
-                var loadEvent = { type: "HypeCardLoad", oldName: oldName, newName: newName };
-                if (hypeFunctions.HypeCardLoad) {
-                    hypeFunctions.HypeCardLoad(hypeDocument, element, loadEvent);
-                } else if (hypeFunctions.HypeCardEvent) {
-                    hypeFunctions.HypeCardEvent(hypeDocument, element, loadEvent);
-                }
-            }
-
-            prevCardByDoc[docId] = newName;
         }
 
-        if (!("HYPE_eventListeners" in window)) {
+        if (!window.HYPE_eventListeners) {
             window.HYPE_eventListeners = [];
         }
+        window.HYPE_eventListeners.push({ type: "HypeScenePrepareForDisplay", callback: HypeScenePrepareForDisplay });
         window.HYPE_eventListeners.push({ type: "HypeSceneLoad", callback: HypeSceneLoad });
 
-        return { version: '1.0.1' };
+        return { version: '1.0.4' };
 
     })();
 }
